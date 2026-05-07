75bae66c369eafec81a89c9781fc9e81c5efcd65
Benjamin Correa <iambenjamin.developer@gmail.com>
2026-05-07 11:07:54 -0300

    constructor 2

--- 01-typescript-intro/src/topics/08-classes.ts
+++ 01-typescript-intro/src/topics/08-classes.ts
@@ -3,22 +3,14 @@ export class Person {
     public age?: number;
     private address: string;
 
-    // constructor() {
-    //     this.name = 'Benjamin';
-    //     this.age = 30;
-    //     this.address = 'Main St';
-    // }
-    // constructor(name: string, age: number, address: string) {
-    //     this.name = name;
-    //     this.age = age;
-    //     this.address = address;
-    // }
 
-    constructor(name: string, address?: string) {
-        this.name = 'Benjamin';
-        this.address = 'Argentina';
+    constructor(name: string, address: string, age?: number) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
     }
+
 }
 
-const ironman = new Person('', 'Costa Rica');
+const ironman = new Person('Benjamin', 'Costa Ricas');
 console.log(ironman);
